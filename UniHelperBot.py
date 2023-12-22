e06eb286ea82f5de469a94ddec913784fd8e0560
krassilnikov <153736692+krassilnikov@users.noreply.github.com>
2023-12-22 16:49:44 +0300

    Update UniHelperBot.py

--- UniHelperBot.py
+++ UniHelperBot.py
@@ -1,71 +1,73 @@
+import os
+import subprocess
 import logging
 import telebot
 from telebot import types
 from txt2audio import text_to_audio
 from audio2text import audio_to_text
+from telebot.types import InputFile
+from pathlib import Path
+
+h_teg = ' '
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # Define the Telegram bot token
-TOKEN = ''
+TOKEN = '6920375156:AAEKC-Q4hiZRNLBWS2IOT4r_2LVse13D9gU'
 
 # Initialize the bot
 bot = telebot.TeleBot(TOKEN)
 
-# Define modes
-TEXT_TO_AUDIO_MODE = 'text_to_audio'
-AUDIO_TO_TEXT_MODE = 'audio_to_text'
-
-# Initial mode
-current_mode = TEXT_TO_AUDIO_MODE
 
 # Command handler for /start command
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Welcome to the Text-to-Audio Bot!')
+    bot.reply_to(message, 'Welcome to the UniHelper Bot!')
 
 # Command handler for /switchmode command
 @bot.message_handler(commands=['switchmode'])
 def switch_mode(message):
-    global current_mode
-    if current_mode == TEXT_TO_AUDIO_MODE:
-        current_mode = AUDIO_TO_TEXT_MODE
-        bot.reply_to(message, 'Switched to Audio-to-Text mode. Send me an audio message for conversion.')
-    else:
-        current_mode = TEXT_TO_AUDIO_MODE
-        bot.reply_to(message, 'Switched to Text-to-Audio mode. Send me text or a PDF file for conversion.')
-
-# Message handler for text input or audio input based on the current mode
-@bot.message_handler(func=lambda message: True)
-def handle_input(message):
-    if current_mode == TEXT_TO_AUDIO_MODE:
-        handle_text_input(message)
-    elif current_mode == AUDIO_TO_TEXT_MODE:
-        handle_audio_input(message)
-
-# Message handler for text input
-def handle_text_input(message):
-    text_input = message.text
-    # Replace the following line with the actual logic from your text_to_audio module
-    audio_waveform = text_to_audio(text_input)
-    # Send the audio file to the user
-    bot.send_voice(message.chat.id, audio_waveform)
-
+    bot.reply_to(message, 'Switched')
 # Message handler for audio input
-def handle_audio_input(message):
-    # Download the audio file
-    if message.voice and message.voice.file_id:
-        audio_file = bot.get_file(message.voice.file_id)
-        audio_file.download('input_audio.ogg')
+@bot.message_handler(content_types=['voice'])
+def voice_processing(message):
+#    bot.reply_to(message, 'Audio-to-Text')
+    os.remove('output_audio.wav')
+    file_id = message.voice.file_id
+    file = bot.get_file(file_id)
+    file_path = file.file_path
+    downloaded_file = bot.download_file(file_path)
+    with open('input_audio.ogg', 'wb') as new_file:
+        new_file.write(downloaded_file)
+    subprocess.run(["ffmpeg-git-20231128-amd64-static/ffmpeg",
+             "-loglevel", "quiet",
+             "-i", "input_audio.ogg",
+             "-ar", "16000",   # частота выборки
+             "-ac", "1",       # кол-во каналов
+             "-f", "s16le",    # кодек для перекодирования в wav
+             "-", "output_audio.wav"]) 
+    audio_text = audio_to_text('output_audio.wav')
+    try:
+        result_text = audio_text["text"]+h_teg 
+        bot.reply_to(message, result_text)
+#        bot.reply_to(message, h_teg)
 
-    # Use audio_to_text module to convert audio to text
-    audio_text = audio_to_text('input_audio.ogg')
+    except KeyError as ke:
+        bot.reply_to(message, 'error1')
 
-    # Replace the following line with the actual logic from your text_to_audio module
-    audio_waveform = text_to_audio(audio_text)
-    # Send the audio file to the user
-    bot.send_voice(message.chat.id, audio_waveform)
+# Message handler for text input 
+@bot.message_handler(content_types=['text'])
+def voice_processing(message):
+#    bot.reply_to(message, 'Text-to-audio')
+    text_input = message.text
+    if "#" in text_input:
+        global h_teg
+        h_teg = '\n' + text_input
+        bot.reply_to(message, h_teg)
+    else:
+        audio_waveform = text_to_audio(text_input)
+        bot.send_voice(message.chat.id, InputFile(audio_waveform))
 
 # Polling loop to keep the bot running
 bot.polling(none_stop=True)
